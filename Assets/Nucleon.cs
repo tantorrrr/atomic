46b5a8f5f430d2d85a5be7324d5891b17cda6b98
lam.phung@exoty.com <lam.phung@exoty.com>
2017-11-07 22:16:40 +0700

    init

---
+++ Assets/Nucleon.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Nucleon : MonoBehaviour 
+{
+    public float AttractionForce;
+    Rigidbody Body;
+
+    void Awake()
+    {
+        Body = GetComponent<Rigidbody>();
+    }
+
+	// Use this for initialization
+	void Start () 
+    {
+
+        //Body.AddForce(transform.localPosition * -AttractionForce * 0.1f, ForceMode.VelocityChange);
+        //zzdebug.log(transform.localPosition * -AttractionForce);
+	}
+
+    
+	
+	void FixedUpdate () 
+    {
+        Body.AddForce(transform.localPosition *-AttractionForce);
+	}
+}
+
+
+public static class zzdebug
+{ 
+    public static void log(Vector3 vector)
+    {
+        Debug.Log("x: " + vector.x + "  y:" + vector.y + "  z:" + vector.z);
+    }
+
+}
---
+++ Assets/NucleonSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NucleonSpawner : MonoBehaviour {
+
+    public float timeBetweenSpawns;
+    public float distanceSpawns;
+    public Nucleon[] prefabs;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    float lastTimeSinceSpawn;
+	void FixedUpdate ()
+    {
+        lastTimeSinceSpawn += Time.deltaTime;
+	    if(lastTimeSinceSpawn >= timeBetweenSpawns)
+        {
+            lastTimeSinceSpawn -= timeBetweenSpawns;
+            SpawnNucleon();
+        }
+	}
+
+    private void SpawnNucleon()
+    {
+        Nucleon prefab = prefabs[Random.Range(0, prefabs.Length)];
+        Nucleon spawn = Instantiate<Nucleon>(prefab);
+        spawn.transform.localScale = Vector3.one;
+        spawn.transform.localPosition = Random.onUnitSphere * distanceSpawns;
+    }
+}
